3216c372ea49f8dedc229dd2da6cee203ffbd19d
AlexisGorospe <156434534+AlexisGorospe@users.noreply.github.com>
2024-10-07 17:00:01 -0400

    i think the thing that creates new users is broken

--- index.js
+++ index.js
@@ -3,46 +3,71 @@ const app = express();
 const router = express.Router();
 
 let users = require('./users.json');
+let employees = require('./employees.json');
 
 router.post("/user/signup/:username/:password", (req, res) => { //BROKEN
     const {username, password} = req.params;
 
+    //console.log(Object.keys(users).length)
 
-    console.log(Object.keys(users).length)
-
-    console.log(users)
+    //console.log(users)
 
     let alreadyExists = false;
     let count = 0;
     if (users.length != 0){
-        for (var i = 0; i < users.length; i++){
+        for (user in users){
             count++
-            if (users[i]["username"] == username){
-                console.log(users[i]["username"])
+            console.log("loop " + user)
+            if (user["username"] == username){
+                console.log(user["username"])
                 alreadyExists = true;
                 break
                     
+            }
+            else {
+                console.log("nope")
             }
         }
     }
 
     if(!alreadyExists){
-        users = {...users, 
+        
+        /*
+        usr5 = { 
             "username": username,
             "password": password 
         };
-        
-        console.log(users)
+        */
 
-        res.status(201).send("You, " + username + ", have created a new account.")
+        let userId = "usr" + count
+        
+        users = {...users,             
+            usr5:{ 
+                "username": username,
+                "password": password 
+            }
+        }
+        
+
+        res.status(201).send(username + " has been added.")
     }
     else{
         res.status(201).send(username + " already exists on the server!");     
     }
 
-
+    console.log(users)
 });
 
+router.get("/emp/employees", (req, res) => { //this one works
+    res.status(200)
+    res.sendFile(__dirname + "/employees.json");
+});
+
+// /api/v1/emp/employees/{eid}
+router.get("/emp/employees/:id", (req, res) => { //this one works
+    res.status(200)
+    res.sendFile(__dirname + "/employees.json");
+});
 
 
 app.use('/', router);
